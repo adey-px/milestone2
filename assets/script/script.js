174435606b92ba731c4dc2efdf318f765da162e5
adey-px <aosteph@gmail.com>
2020-12-08 16:30:38 +0000

    added comments to javascript file

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,4 @@
+ // Code to make number buttons work starts here
 function insert(num){
    var myString = document.form.textview.value;
    var lastChar = myString[myString.length - 1];
@@ -9,16 +10,26 @@ function insert(num){
     }
    }
   }
+// Code to make number buttons work ends here
+
+// Code to make equal button work starts here
   function equal(){
    var sum = document.form.textview.value;
    if (sum) {
     document.form.textview.value = eval(sum);
    }
   }
+// Code to make equal button work ends here
+
+// Code to make clear button work starts here
   function clean(){
    document.form.textview.value = '';
   }
+// Code to make clear button work ends here
+
+// Code to make backspace button work starts here
   function back(){
    var exp = document.form.textview.value;
    document.form.textview.value = exp.substring(0,exp.length - 1);
   }
+// Code to make backspace button work ends here
